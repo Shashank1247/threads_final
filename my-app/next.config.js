b0b551ef08e400531fdbde7c08f4ede3705ceb2e
Shashank <shashank@shashanks-mbp.mynetworksettings.com>
2024-03-07 01:02:12 -0500

    final commit

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,41 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = {};
 
-    typescript: {
-        ignoreBuildErrors : true,
-    },
-
-    //updated for vercel
-    
-    experimental: {
-      serverActions: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+export default nextConfig;
--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 const nextConfig = {
 
   typescript: {
-    ignoreBuildError: true,
+    ignoreBuildErrors: true,
   },
 
   experimental: {
